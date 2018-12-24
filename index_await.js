1633f30d2afa8b37dccdbe6dcc58abbae52954d7
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2018-12-24 17:50:22 +0530

    Finally reached all the elements

--- index_await.js
+++ index_await.js
@@ -14,31 +14,45 @@ var scraper = async () => {
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"
     );
     await page.goto(`https://www.sih.gov.in/sih2019ProblemStatements?page=1`);
-    await page.waitForSelector("#table_id_info");
-    // document.querySelector("#settings > thead > tr:nth-child(1) > td > div");
-    var tableScraper = await page.evaluate(() => {
-      const count = document.querySelector("#table_id_info");
-      var ideaCount = parseInt(count.innerText.trim().slice(13, 15), 10);
-      var list = [];
-      console.log(ideaCount);
-      //   for (let i = 1; i < ideaCount; i++) {
-      let ideaName = document.querySelectorAll(
-        `#settings > thead > tr:nth-child(1) > td > div`
+    // await page.waitForSelector("#table_id_info");
+    var final = await page.evaluate(() => {
+      var ex = document.querySelectorAll(
+        "#settings > thead > tr:nth-child(1) > td >div"
       );
-      for (let i = 1; i < ideaCount; i++) {
-        list.unshift(ideaName[i--].innerText.trim());
-      }
-      // let ideaOrg = document.querySelector(
-      //   `#table_id > tbody > tr:nth-child(${i}) > td:nth-child(2)`
-      // );
-      // let ideaCategory = document.querySelector(
-      //   `#table_id > tbody > tr:nth-child(${i}) > td:nth-child(4)`
-      // );
-      // list.unshift(ideaName.innerText.trim());
-      //   }
-      return list;
+      console.log(ex[1].innerText.trim());
     });
-    console.log(tableScraper);
+
+    // let ideaNames = await page.$$("#settings");
+    // console.log(
+    //   ideaNames[0].$eval("thead", el => {
+    //     console.log(el);
+    //   })
+    // );
+
+    // for (const ideas of ideaNames) {
+    //   ideas.$eval("#settings>thead >tr:nth-child(1)>td", el => {
+    //     console.log(el.innerText);
+    //   });
+    // }
+    // var tableScraper = await page.evaluate(() => {
+    //   const count = document.querySelector("#table_id_info");
+    //   var ideaCount = parseInt(count.innerText.trim().slice(13, 15), 10);
+    //   var list = [];
+    //   console.log(ideaCount);
+    // //   for (let i = 1; i < ideaCount; i++) {
+    //     let ideaName = document.querySelectorAll(`.modal-body`);
+    //     for
+    //     // let ideaOrg = document.querySelector(
+    //     //   `#table_id > tbody > tr:nth-child(${i}) > td:nth-child(2)`
+    //     // );
+    //     // let ideaCategory = document.querySelector(
+    //     //   `#table_id > tbody > tr:nth-child(${i}) > td:nth-child(4)`
+    //     // );
+    //     list.unshift(ideaName.innerText.trim());
+    // //   }
+    //   return list;
+    // });
+    // console.log(tableScraper);
 
     // const cells = await page.$$(
     //   "#table_id > tbody > tr:nth-child(1) > td:nth-child(4)"
@@ -60,3 +74,5 @@ var scraper = async () => {
 };
 
 scraper();
+
+// #settings > thead > tr: nth - child(1) > td
